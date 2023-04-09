7871af99916d79b2caa135871db6429384f64f2b
Surya Narayan <surya.narayan0707@gmail.com>
2023-04-09 16:30:41 -0700

    added blockchain logo

--- src/portfolio.js
+++ src/portfolio.js
@@ -276,7 +276,7 @@ const achievementSection = {
       title: "Game Oasis Hackathon Winners",
       subtitle:
         "Led a team of 3 in building co-op game on Unity using layer 2 blockchain network.",
-      image: require("./assets/images/matic-logo2.webp"),
+      image: require("./assets/images/blockchain-logo.png"),
       imageAlt: "Game Oasis Hackathon",
       footerLink: [
       ]
