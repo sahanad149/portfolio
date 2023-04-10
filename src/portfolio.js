57275cae7fe160de79e35e18303d874247707649
Surya Narayan <surya.narayan0707@gmail.com>
2023-04-10 12:59:53 -0700

    added xbox logo 2

--- src/portfolio.js
+++ src/portfolio.js
@@ -283,7 +283,7 @@ const bigProjects = {
       ]
     },
     {
-      image: require("./assets/images/xbox-logo.jpg"),
+      image: require("./assets/images/xbox-logo2.jpg"),
       projectName: "3d Reconstruction of Plant leaves",
       projectDesc: "Used Xbox Kinect 360, Meshlab and OpenCV to 3d reconstruct a plant and split leaves to track their measurements",
       footerLink: [
