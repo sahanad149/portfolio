6653834bb5769f5113087c20c7887283c5c9b46d
Surya Narayan <surya.narayan0707@gmail.com>
2023-04-16 13:07:35 -0700

    changing logo to make it work ?

--- src/portfolio.js
+++ src/portfolio.js
@@ -370,9 +370,12 @@ const extracurricular = {
       title: "Volunteer for Aikya",
       subtitle:
         "Served food and taught government primary school kids under Akshaya Patra Foundation",
-      image: require("./assets/images/akshaya-logo.jpg"),
-      imageAlt: "ASA DataFest Hackathon Mentor",
-      footerLink: [ 'https://www.linkedin.com/company/the-akshaya-patra-foundation/ '
+      image: require("./assets/images/aikya-logo.png"),
+      imageAlt: "Akshaya Patra Foundation logo",
+      footerLink: [ {
+        name: "Company",
+        url: "https://www.linkedin.com/company/the-akshaya-patra-foundation/"
+      }
       ]
     },
 
