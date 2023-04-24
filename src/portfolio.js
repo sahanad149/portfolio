f245f2247424fdb3d01d02a14e65415ecc480612
Surya Narayan <surya.narayan0707@gmail.com>
2023-04-23 18:31:23 -0700

    changed phone number in portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -491,7 +491,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+1 (602) 815-2050",
+  number: "+1 (480) 736-0012",
   email_address: "surya.narayan@asu.edu"
 };
 
